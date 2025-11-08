db0f9c02105dfeaa4c08116e5e7a2e91a68bdf5e
Maximiliano <maximiliano.uribe@userena.cl>
2025-11-08 18:34:20 -0300

    Add initial comments to control.py
    Add initial comments for project functionality.

---
+++ codigos/control.py
@@ -0,0 +1,3 @@
+"""aqui entra el codigo principal de este proyecto, estara todo lo relacionado a su funcionalidad 
+llevara todo para poder ser mas facil de editar sin muchos cambios o sin complicidad"""
+
